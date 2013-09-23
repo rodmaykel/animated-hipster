16ed536740fc970b72fb6bb53b9d694da260deac
Rod Michael Coronel <rodmaykel@gmail.com>
2013-09-23 22:29:16 +0800

    First commit with passing test

---
+++ test/index.test.js
@@ -0,0 +1,42 @@
+var assert = require('assert');
+var restler = require('restler');
+
+var LOG = true;
+
+function log(str){
+  if (LOG === true) {
+    console.log(str);
+  }
+}
+
+
+describe('Index', function(){
+
+  before(function(){
+    log("Index test: started");
+  }) // before
+  
+  describe('#index_success', function(){
+    before(function(){
+      log("index_success: started");
+    }) // before
+
+
+    beforeEach(function(){
+      log("===================================================");
+    }) // before
+    
+    
+    it('index should response success:1', function(done){
+      
+      done();
+    }) // it
+    
+    
+    after(function(done) {
+      log("Index test: finished");
+      done();
+    }) // after
+
+  }); // describe
+});
