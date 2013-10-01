738a8449f2375a984d191201b71ba6e6046bf364
Rod Michael Coronel <rodmaykel@gmail.com>
2013-10-01 20:56:39 +0800

    Add actual test for the index using supertest

--- app.js
+++ app.js
@@ -31,3 +31,5 @@ app.get('/', routes.index);
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+module.exports = app;
